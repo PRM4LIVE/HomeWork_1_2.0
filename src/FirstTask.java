a4e97816c8697b9947241137b18276c80a0075dd
Dmitry <wasdwas@mail.ru>
2021-07-06 00:27:04 +0300

    FirstTask исправлена ошибка ввода букв и неправильного ввода дробных чисел1

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -7,16 +7,16 @@ public class FirstTask {
         try {
             double a = scanner.nextDouble(); //сделать возможным ввод текста
 
-        if (a == 3) {
-            System.out.println("Три");
-        } else if (a == 7) {
-            System.out.println("Семь");
-        } else {
+            if (a == 3) {
+                System.out.println("Три");
+            } else if (a == 7) {
+                System.out.println("Семь");
+            } else {
+                System.out.println("Что-то другое");
+            }
+        } catch (Exception e) {
             System.out.println("Что-то другое");
         }
-        }
-        catch (Exception e) {
-            System.out.println("Что-то другое");}
     }
 }
 

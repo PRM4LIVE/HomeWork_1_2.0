7eebb1ab37533885c4673cea4ce1cefea02bd393
Dmitry <wasdwas@mail.ru>
2021-07-06 01:04:59 +0300

    Cinema, добавлен Scanner, добавлены ограничения из условия задачи.

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class Cinema {
     public static void main(String[] args) {
-        int a = 2; //Количество новых фильмов
-        int b = 1500; //Количество денег у Макса
-        int c = 650; //Стоимость интернета
-        int sum = a * 250 + c; //Необходимое количество денег
-        if (sum <= b) {
-            System.out.println("YES");
-        }
-        else {
-            System.out.println("NO");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество фильмов которые хочет посмотреть Макс (от 0 до 20):");
+        int n = scanner.nextInt();//Количество новых фильмов
+        if (0 <= n & n <= 20) {
+            System.out.println("Введите сумму на карте Макса (от 0 до 6000):");
+            int m = scanner.nextInt(); //Количество денег у Макса
+            if (0 <= m & m <= 6000) {
+                int c = 650; //Стоимость интернета
+                int sum = n * 250 + c; //Необходимое количество денег
+                if (sum <= m) {
+                    System.out.println("YES");
+                } else System.out.println("NO");} else System.out.println("Сумма на карте Макса должна быть от 0 до 6000");
+            } else System.out.println("Количство фильмов должно быть от 0 до 20");
         }
     }
-}
